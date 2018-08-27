860f4170ab56e68fb29101ffa0604403db58d937
Valentyn Khalin <send.mail.to.flash@gmail.com>
2018-08-27 14:53:22 +0300

    reformatted code, added simple exception handling, correct regex, auto-comparator for all files

--- src/main/scala/FileCompare.scala
+++ src/main/scala/FileCompare.scala
@@ -12,19 +12,30 @@ object FileCompare {
 
     def main(args: Array[String]): Unit = {
 
-        val dir = new File(System.getProperty("user.dir") + "/testfiles/")
-        var stringsList = ListBuffer.empty[String]
-        val compareStrings = new Levenshtein
+        try {
 
+            val dir = new File(System.getProperty("user.dir") + "/testfiles/")
+            var stringsList = ListBuffer.empty[String]
+            val compareStrings = new Levenshtein
 
-        for(file_ <- filesIterator(dir)) {
-            var lines = Source.fromFile(file_).getLines.mkString
-                .toLowerCase.replaceAll("[^A-Za-z0-9 ]", "")
-            stringsList += lines
+            for(file_ <- filesIterator(dir)) {
+                var lines = Source.fromFile(file_).getLines.mkString
+                    .toLowerCase.replaceAll("[^A-Za-z0-9 ]", "")
+                stringsList += lines
+            }
+
+            //println(compareStrings.distance(stringsList.head, stringsList(2)))
+
+            // loop to compare elements with each other
+            for (a <- stringsList; b <- stringsList) {
+                println(("Levenshtein distance between Strings:\n" +
+                    "1) %s\n" +
+                    "2) %s\n" +
+                    "Equals: %s\n").format(a, b, compareStrings.distance(a, b)))
+            }
+
+        } catch {
+            case ex: NullPointerException => ex.printStackTrace()
         }
-
-        // comparison works but need to compare all files
-        // automatically between each other
-        println(compareStrings.distance(stringsList.head, stringsList(2)))
     }
 }
